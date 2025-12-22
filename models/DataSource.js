c6fdd91041a3d7b2df1d2683229dfea83f1fe9e4
edwardskcmo <134224824+edwardskcmo@users.noreply.github.com>
2025-12-22 17:16:44 -0600

    Update DataSource.js

--- models/DataSource.js
+++ models/DataSource.js
@@ -1,24 +1,21 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
-
-const DataSource = sequelize.define('DataSource', {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    type: {
-        type: DataTypes.STRING,
-        defaultValue: 'Document'
-    },
-    url: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    // NEW: This is where we will store the text extracted from your files
-    content: {
-        type: DataTypes.TEXT,
-        allowNull: true 
-    }
-});
-
-module.exports = DataSource;
+module.exports = (sequelize, DataTypes) => {
+    const DataSource = sequelize.define('DataSource', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        type: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        url: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        }
+    });
+    return DataSource;
+};
