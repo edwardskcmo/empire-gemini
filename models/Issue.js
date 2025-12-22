90b76405a27289292d4cf9b7e9dae29fc39e85e9
edwardskcmo <134224824+edwardskcmo@users.noreply.github.com>
2025-12-22 17:17:15 -0600

    Update Issue.js

--- models/Issue.js
+++ models/Issue.js
@@ -1,27 +1,25 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
-
-const Issue = sequelize.define('Issue', {
-    description: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    department: {
-        type: DataTypes.STRING,
-        defaultValue: 'General'
-    },
-    priority: {
-        type: DataTypes.STRING,
-        defaultValue: 'Normal'
-    },
-    status: {
-        type: DataTypes.STRING,
-        defaultValue: 'Open'
-    },
-    assignee: {
-        type: DataTypes.STRING,
-        defaultValue: 'Unassigned'
-    }
-});
-
-module.exports = Issue;
+module.exports = (sequelize, DataTypes) => {
+    const Issue = sequelize.define('Issue', {
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        priority: {
+            type: DataTypes.STRING,
+            defaultValue: 'Normal'
+        },
+        status: {
+            type: DataTypes.STRING,
+            defaultValue: 'Open'
+        },
+        department: {
+            type: DataTypes.STRING,
+            defaultValue: 'General'
+        },
+        assignee: {
+            type: DataTypes.STRING,
+            defaultValue: 'Unassigned'
+        }
+    });
+    return Issue;
+};
