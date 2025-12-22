1e1ae682fdd425efb3aa4e41a64d2316878c89f8
edwardskcmo <134224824+edwardskcmo@users.noreply.github.com>
2025-12-22 17:16:14 -0600

    Update ChatMessage.js

--- models/ChatMessage.js
+++ models/ChatMessage.js
@@ -1,19 +1,17 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
-
-const ChatMessage = sequelize.define('ChatMessage', {
-    role: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    content: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    timestamp: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW
-    }
-});
-
-module.exports = ChatMessage;
+module.exports = (sequelize, DataTypes) => {
+    const ChatMessage = sequelize.define('ChatMessage', {
+        role: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        timestamp: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        }
+    });
+    return ChatMessage;
+};
